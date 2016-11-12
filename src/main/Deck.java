650ea9978ff07c343d96e6760490309c2df96f0b
rf222cz <rf222cz@student.lnu.se>
2016-11-12 13:48:50 +0100

    :blue_book: Refactor - Create Cards

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by ymafr on 2016-11-12.
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 public class Deck {
     private int maxCards;
     private ArrayList<Character> alphabet = new ArrayList<Character>();
+    private ArrayList<Character> usedLetters = new ArrayList<Character>();
     private ArrayList<Card> cards = new ArrayList<Card>(maxCards);
 
     public Deck(int maxCards) {
@@ -26,9 +28,29 @@ public class Deck {
         }
     }
 
+    //Takes first letter from the alphabet
+    //Adds the letter to the usedletters array
     private void createCardsFirstHalf() {
+        Random rand = new Random();
+        int id = rand.nextInt(alphabet.size());
+
+        char letter = this.alphabet.get(id);
+        this.usedLetters.add(letter);
+        this.alphabet.remove(id);
+
+        Card newCard = new Card(letter, id);
+        cards.add(newCard);
     }
+    //When half of the maxCards has gone then take from the used letters insted
     private void createCardsSecondHalf() {
+        Random rand = new Random();
+        int id = rand.nextInt(usedLetters.size());
+
+        char letter = this.usedLetters.get(id);
+        this.usedLetters.remove(id);
+
+        Card newCard = new Card(letter, id);
+        cards.add(newCard);
     }
 
 
