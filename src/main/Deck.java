93794dd346e50d516c3048611fe937c44907df85
rf222cz <rf222cz@student.lnu.se>
2016-11-12 13:20:16 +0100

    :red_circle: Red - Test Set Alphabet

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -1,10 +1,13 @@
 package main;
 
+import java.util.ArrayList;
+
 /**
  * Created by ymafr on 2016-11-12.
  */
 public class Deck {
     private int maxCards;
+    private ArrayList<Character> alphabet;
 
     public Deck(int maxCards) {
         this.maxCards = maxCards;
@@ -12,5 +15,9 @@ public class Deck {
 
     public int getMaxCards() {
         return maxCards;
+    }
+
+    public ArrayList<Character> getAlphabet() {
+        return alphabet;
     }
 }
--- src/test/TestDeck.java
+++ src/test/TestDeck.java
@@ -4,6 +4,10 @@ import main.Deck;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by ymafr on 2016-11-12.
  */
@@ -21,5 +25,28 @@ public class TestDeck {
         Deck deck = new Deck(expectedMaxCards);
 
         Assert.assertEquals(deck.getMaxCards(), expectedMaxCards);
+    }
+
+    @Test
+    public void testSetAlphabet(){
+        System.out.println("testSetAlphabet()");
+        Deck deck = new Deck(10);
+
+        ArrayList<Character> expectedAlphabet = createAlphabet();
+
+
+        Assert.assertEquals(deck.getAlphabet(), expectedAlphabet);
+    }
+
+
+
+    //Help methods
+    private ArrayList<Character> createAlphabet(){
+        String letters = "abcdefghijklmnopqrstvuwz";
+        ArrayList<Character> chars = new ArrayList<Character>();
+        for (char c : letters.toCharArray()) {
+            chars.add(c);
+        }
+        return chars;
     }
 }
