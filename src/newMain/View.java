f1be4ad19a91f8a74c3d23a74b3675d99769369f
rf222cz <rf222cz@student.lnu.se>
2016-11-16 13:33:21 +0100

    :green_heart: Green - test print Start

--- src/newMain/View.java
+++ src/newMain/View.java
@@ -1,18 +1,21 @@
 package newMain;
 
+import java.io.PrintStream;
+
 /**
  * Created by ymafr on 2016-11-16.
  */
 public class View {
 
-    private StringBuffer stringBuffer;
+    private PrintStream out;
 
     public final static String START = "Welcome, let's start! \nPick a even number between 2 and 40:";
 
+    public View(PrintStream out){
+        this.out = out;
+    }
     public void printStart() {
+        out.println(START);
     }
 
-    public String getStringBuffer() {
-        return stringBuffer.toString();
-    }
 }
--- src/newTest/ViewTest.java
+++ src/newTest/ViewTest.java
@@ -4,6 +4,11 @@ import newMain.View;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 
 /**
  * Created by ymafr on 2016-11-16.
@@ -15,11 +20,12 @@ public class ViewTest {
     @Test
     public void testPrintStart(){
         System.out.println("testStart()");
+        PrintStream ps = mock(PrintStream.class);
+        view = new View(ps);
 
         view.printStart();
 
-        Assert.assertEquals(view.getStringBuffer(), view.START);
-
+        verify(ps).println(view.START);
     }
 
 }
